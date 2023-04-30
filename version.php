998f4196724b1cd7810f041c37344b597f517036
ThreeAi <alexandrovaa2003@gmail.com>
2023-04-30 22:28:50 +0300

    reverse

--- version.php
+++ version.php
@@ -2,6 +2,6 @@
 defined('MOODLE_INTERNAL') || die();
 $plugin->version = 202112400; 
 $plugin->requires = 2014051200;
-$plugin->component = 'local_plugin_get_field';
+$plugin->component = 'mod_get_field';
 $plugin->release   = '0.0.1';
 $plugin->maturity  = MATURITY_ALPHA;
