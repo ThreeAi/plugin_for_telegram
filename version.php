fceb976ed5367fce502fb3937aee76baa9a2dd4e
ThreeAi <alexandrovaa2003@gmail.com>
2023-04-30 22:38:33 +0300

    4

--- version.php
+++ version.php
@@ -2,6 +2,6 @@
 defined('MOODLE_INTERNAL') || die();
 $plugin->version = 202112400; 
 $plugin->requires = 2014051200;
-$plugin->component = 'mod_get_field';
+$plugin->component = 'local_plugin_for_telegram';
 $plugin->release   = '0.0.1';
 $plugin->maturity  = MATURITY_ALPHA;
