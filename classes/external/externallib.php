b09387f50e1e95de6d8fe9a9b400f22994bdc878
ThreeAi <alexandrovaa2003@gmail.com>
2023-05-03 00:14:33 +0300

    fix returned param

--- classes/external/externallib.php
+++ classes/external/externallib.php
@@ -13,31 +13,15 @@ class get_user_by_field_tg_external extends external_api {
     public static function get_user_by_field_tg($username_tg) {
         global $DB;
         $users = array();
-        $users = $DB->get_records_list('user', 'WHERE skype = ?', $username_tg, 'id');
+        $users = $DB->get_records_list('user', 'skype', [$username_tg], 'id');
         if (!empty($users)) {
             $user = reset($users); 
-            return self::user_returns($user);
+            return $user->id;
         } else {
             return null;
         }
     }
-
-
     public static function get_user_by_field_tg_returns() {
-        return new external_multiple_structure(self::user_returns($user));
-    }
-    
-    private static function user_returns($user) {
-        $user_fields = array(
-            'id' => new external_value(PARAM_INT, 'User ID'),
-            'username' => new external_value(PARAM_TEXT, 'Username'),
-            'email' => new external_value(PARAM_TEXT, 'Email'),
-            'firstname' => new external_value(PARAM_TEXT, 'First name'),
-            'lastname' => new external_value(PARAM_TEXT, 'Last name'),
-            'fullname' => new external_value(PARAM_TEXT, 'Full name'),
-            'skype' => new external_value(PARAM_TEXT, 'Skype'),
-        );
-
-        return new external_single_structure($user_fields);
+        return new external_value(PARAM_RAW);
     }
 }
